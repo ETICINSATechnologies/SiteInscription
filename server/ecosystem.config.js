0c56ac0a71d09cccdf20a0c5c428c20a3c935381
sadsithalokuge <slokuge97@gmail.com>
2019-09-30 22:42:15 +0200

    Reducing concurrent instances to 1

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'API',
     script: 'app.js',
-    instances: 2,
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G'
